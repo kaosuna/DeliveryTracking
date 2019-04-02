e42627e172a3200c278b897f2dfcc2c94fb71a03
stmkza <stmkza@gmail.com>
2019-04-02 19:01:39 +0900

    DeliveryItemStatusの実装メソッドを書いてみた

--- src/JapanDeliveryTracker/DeliveryItemStatus.php
+++ src/JapanDeliveryTracker/DeliveryItemStatus.php
@@ -6,5 +6,17 @@ namespace Pasuke\JapanDeliveryTracker;
 
 interface DeliveryItemStatus
 {
+    /**
+     * 配送状態を取得する
+     *
+     * @return DeliveryState 荷物の配送状態
+     */
+    public function getDeliveryState(): DeliveryState;
 
+    /**
+     * 荷物の追跡番号を取得する
+     *
+     * @return TrackingCode 荷物の追跡番号
+     */
+    public function getTrackingCode(): TrackingCode;
 }
